0d1f219eb9b9451f6ef2b0922cf97945e81e1dd3
ISAsxm <huguesisabelle82@gmail.com>
2021-06-04 12:05:11 +0200

    Gauss_v2.py

---
+++ ex07/Gauss_v2.py
@@ -0,0 +1,13 @@
+nbrInput = int(input ("Quel est votre nombre ? :\n"))
+
+def gaus(nbr_input):
+        nbr = 1
+        nbrSomme = 1
+        while (nbr < nbr_Input) :
+                nbr += 1
+                nbrSomme += nbr
+
+        print (f"Nous allons calculer la somme de tous les nombre de 1 jusqu'à $
+
+gaus(nbrInput
+)
